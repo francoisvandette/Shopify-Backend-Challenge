b1eb59c2df7903c5de32935cec173e1e3fef5c5b
Francois Vandette <100052703+francoisvandette@users.noreply.github.com>
2022-05-17 17:26:51 -0700

    Final2

--- server/app2.js
+++
@@ -1,9 +0,0 @@
-const connection = require(`../dbService`);
-
-async function getAll() {
-    const sql = "SELECT * FROM names";
-    const [rows] = await connection.promise().query(sql);
-    return rows;
-} 
-
-console.table(getAll());

aedd472f4be4065e0317370c66d625b4a2c79838
Francois Vandette <100052703+francoisvandette@users.noreply.github.com>
2022-05-18 19:22:26 -0700

    readme and load test data
    - load test data button disables after it is pressed, to prevent multiple presses by accident.
    - readme created to explain project.

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ let deleteData = document.querySelector(`#delete-all-data`);
 loadData.addEventListener(`click`, async function() {
     await loadTestData();
     alert(`Test data loaded`);
+    loadData.disabled = true;
 });
 
 deleteData.addEventListener(`click`, async function() {
     await deleteAllData();
     alert(`All data in the database was deleted.`);
+    loadData.disabled = false;
 })
